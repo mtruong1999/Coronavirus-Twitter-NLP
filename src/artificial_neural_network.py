3dee6009dcdcad215c5135399502dc618ea0d234
mllouie <mllouie@unm.edu>
2020-11-27 00:43:24 -0700

    starting ann

--- src/artificial_neural_network.py
+++ src/artificial_neural_network.py
@@ -3,7 +3,7 @@ import sys
 
 class ArtificialNeuralNetwork:
 
-    def __init__(self, dataDirPath, idSentiments):
+    def __init__(self):
         """
         Trains classifier using the training data in dataDirPath and
         uses the resulting model to classify the test data in dataDirPath.
@@ -11,4 +11,18 @@ class ArtificialNeuralNetwork:
         will be left empty if using validation
         """
         pass
-    
+
+    def __call__(self, dataDirPath, idSentiments):
+        pass
+
+    def nn(self):
+        pass
+
+    def train_model(self, model):
+        pass
+
+    def validate(self, model):
+        pass
+
+    def test_model(self, model):
+        pass
